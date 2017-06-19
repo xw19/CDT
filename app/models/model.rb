cd816aff3ea776c1d397f9518757447265106dfd
Sourav Moitra <sourav.moitr@gmail.com>
2017-06-19 23:54:48 +0530

    Some more changes

--- app/models/model.rb
+++ app/models/model.rb
@@ -5,4 +5,12 @@ class Model < ApplicationRecord
 
   validates :price, presence: true, numericality: { only_integer: true }
   validates :year, presence: true, numericality: { only_integer: true }
+
+
+  def master_picture
+    if pictures.any?
+      return "https://cartruckdealerships.herokuapp.com" + pictures.first.image_url
+    end
+    "http://lorempixel.com/400/300/transport/3/"
+  end
 end
--- app/views/api/v1/dealerships/show.json.jbuilder
+++ app/views/api/v1/dealerships/show.json.jbuilder
@@ -5,6 +5,7 @@ json.models @dealership.models do |model|
   json.id model.id
   json.brand model.car.make.name
   json.name  model.car.model
+  json.thumb model.master_picture
   json.year model.year
   json.price number_to_currency(model.price, currency: '$')
 end
